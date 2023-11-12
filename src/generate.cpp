1e52edf6a7e7ee4e60c043bf4894ef3e9dce0076
Dimitrije Dobrota <mail@dimitrijedobrota.com>
2023-11-12 21:47:15 +0000

    Add generator program to be used with many files

--- src/generate.cpp
+++ src/generate.cpp
@@ -1,22 +1,7 @@
-#include "json.hpp"
-#include <format>
-#include <fstream>
-#include <iostream>
-#include <string>
-
-using json = nlohmann::json;
+#include "menu.h"
 
 int main(const int argc, const char *argv[]) {
-  for (int i = 1; i < argc; i++) {
-    const char *filename = argv[i];
-    for (const auto &menu : json::parse(std::fstream(filename))) {
-      std::cout << std::format("{}({}):\n", (std::string)menu["name"],
-                               (std::string)menu["code"]);
-      for (const auto &item : menu["items"]) {
-        std::cout << std::format("* {}({})\n", (std::string)item["prompt"],
-                                 (std::string)item["callback"]);
-      }
-    }
-  }
+  for (int i = 1; i < argc; i++) Menu::read(argv[i]);
+  Menu::generate();
   return 0;
 }
